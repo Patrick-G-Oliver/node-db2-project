3a858eb3fbb386e6ad5cf792f2547726b612f1d4
Patrick-G-Oliver <patrickgriffitholiver@gmail.com>
2020-10-18 21:04:10 -0400

    procedural summary added to index.js

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+// Project Procedures Summary:
+// Step 1: Set up server in index.js.
+// Step 2: Set up knexfile.js.
+// Step 3: Set up data/config.js.
+// Step 4: Set up migrations folder with: npx knex migrate:make car-dealer
+// Step 5: Build data table in migration file created in step 4. 
+// Step 6: Create cars.db3 data-table file by running: npx knex migrate:latest
+// Step 7: Check out the newly-created table on TablePlus (SQL connection).
+// Step 8: Set up a seed data file to populate the table and test the soon-to-be-created
+// 		   router endpoints (on Insomnia) with: npx knex seed:make cars 
+//	       (this creates the cars.js file in the seeds folder).
+// Step 9: Write up the seed data (as appears in seeds/cars.js).
+// Step 10: Populate the car-dealer table with seed file data by running:
+//          npx knex seed:run
+// Step 11: Check out the now-populated table in TablePlus.
+// Step 12: Set up the router (cars/cars-router.js) to perform CRUD operations 
+//          and test out on Insomnia. 
+
 const express = require("express")
 const welcomeRouter = require("./welcome/welcome-router")
 const carsRouter = require("./cars/cars-router")
